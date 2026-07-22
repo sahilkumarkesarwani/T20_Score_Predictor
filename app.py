4192f3cd49dc7a8e8d2dc1519658844e2b0c922c
Sahil Kumar <sahilkumarkesarwani316@gmail.com>
2026-07-22 21:15:52 +0530

    first commit

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if batting_team == bowling_team:
 col3, col4, col5 = st.columns(3)
 
 with col3:
-    overs = st.number_input('How many overs have been played', min_value = 0.1, max_value= 20.0, step = 1)
+    overs = st.number_input('How many overs have been played', min_value = 1, max_value= 20, step = 1)
 
 with col4:
     runs = st.number_input('How many runs have been scored', max_value= 360, steps = 1)
